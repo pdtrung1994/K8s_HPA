b56b7d1e11e9b94b60aa78584e3a4641df80c3fb
Pham Duc Trung <33346418+pdtrung1994@users.noreply.github.com>
2021-11-18 06:09:46 +0100

    update

--- app.py
+++ app.py
@@ -7,44 +7,44 @@ app = Flask(__name__)
 CORS(app)
 
 
-# def calc_pi():
-#     # Initialize denominator
-#     k = Decimal(1)
+def calc_pi():
+    # Initialize denominator
+    k = Decimal(1)
  
-#     # Initialize sum
-#     s = Decimal(0)
+    # Initialize sum
+    s = Decimal(0)
  
-#     for i in range(1000000):
+    for i in range(1000000):
  
-#         # even index elements are positive
-#         if i % 2 == 0:
-#             s += Decimal(4)/Decimal(k)
-#         else:
+        # even index elements are positive
+        if i % 2 == 0:
+            s += Decimal(4)/Decimal(k)
+        else:
  
-#         # odd index elements are negative
-#             s -= Decimal(4)/Decimal(k)
+        # odd index elements are negative
+            s -= Decimal(4)/Decimal(k)
  
-#         # denominator is odd
-#         k += Decimal(2)
-#     return s
-#     # return sum(1 / Decimal(16) ** k *
-#     #     (Decimal(4) / (8 * k + 1) -
-#     #      Decimal(2) / (8 * k + 4) -
-#     #      Decimal(1) / (8 * k + 5) -
-#     #      Decimal(1) / (8 * k + 6)) for k in range(100))
-#     # return 1
+        # denominator is odd
+        k += Decimal(2)
+    return s
+    # return sum(1 / Decimal(16) ** k *
+    #     (Decimal(4) / (8 * k + 1) -
+    #      Decimal(2) / (8 * k + 4) -
+    #      Decimal(1) / (8 * k + 5) -
+    #      Decimal(1) / (8 * k + 6)) for k in range(100))
+    # return 1
 
-def runcalc():
-    x = Decimal(0)
-    for i in range(30000000):
-        x = math.sqrt(i)
-    return x
+# def runcalc():
+#     x = Decimal(0)
+#     for i in range(30000000):
+#         x = math.sqrt(i)
+#     return x
 
 @app.route('/')
 def dashboard():
-    getcontext().prec = 10
-    # return str(calc_pi())
-    return str(runcalc())
+    getcontext().prec = 1000000
+    return str(calc_pi())
+    # return str(runcalc())
 
 
 if __name__ == '__main__':
