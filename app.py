7a5db957efa8e6df6e5654449e16e474b5180496
Pham Duc Trung <33346418+pdtrung1994@users.noreply.github.com>
2021-11-20 10:05:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def calc_pi():
     # Initialize sum
     s = Decimal(0)
  
-    for i in range(1000000):
+    for i in range(200000):
  
         # even index elements are positive
         if i % 2 == 0:
@@ -42,7 +42,7 @@ def calc_pi():
 
 @app.route('/')
 def dashboard():
-    getcontext().prec = 500
+    getcontext().prec = 100
     return str(calc_pi())
     # return str(runcalc())
 
